e43ef54086f5f0e99d0e39285602195214d1a160
Muhammad Anisuzzaman <anisseam238@gmail.com>
2022-09-16 23:58:01 +0600

    Design Result Page

---
+++ src/components/pages/Result.js
@@ -0,0 +1,11 @@
+import Analysis from "../Analysis";
+import Summary from "../Summary";
+
+export default function Result(props) {
+    return (
+        <>
+            <Summary />
+            <Analysis />
+        </>
+    );
+}
